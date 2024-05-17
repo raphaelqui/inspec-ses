e35dbbc99ef810a8f387ccc27067340beffa55e4
raphaelqui <quinto.raph9@gmail.com>
2024-05-17 14:54:08 +0200

    buildable

--- functions/helper.ts
+++ functions/helper.ts
@@ -1,10 +1,10 @@
 import * as CryptoJS from 'crypto-js';
 
-export const encryptMsg = (plaintext: string, passphrase: string)=>{
+export const encryptMsg = (plaintext: string, passphrase: any)=>{
     return CryptoJS.AES.encrypt(plaintext, passphrase).toString();
 }
 
-export const decryptMsg = (ciphertext: string, passphrase: string)=>{
+export const decryptMsg = (ciphertext: string, passphrase: any)=>{
     const bytes = CryptoJS.AES.decrypt(ciphertext, passphrase);
     const decryptedText = bytes.toString(CryptoJS.enc.Utf8);
     return decryptedText;	
